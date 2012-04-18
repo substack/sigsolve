fa89c0256cf56ad9ce1fdf673c7d6e3cedd89d29
James Halliday <mail@substack.net>
2012-04-18 15:05:11 -0700

    failing test for higher-order functions

---
+++ test/fn.js
@@ -0,0 +1,17 @@
+var test = require('tap').test;
+var sigsolve = require('../');
+
+test('multiple elements in a type signature', function (t) {
+    var pool = {
+        f : [ [ 'a', 'b' ], 'c' ],
+        g : [ 'c', 'z' ],
+        h : [ 'a', 'd' ],
+        i : [ 'd', 'b' ],
+    };
+    t.same(
+        sigsolve([ 'a', 'z' ], pool),
+        [ [ [ 'f', [ 'h', 'i' ] ], 'g' ] ]
+    );
+    
+    t.end();
+});
