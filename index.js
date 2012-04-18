634752e2afecd7c24a575f7f2f463b981bbaf873
James Halliday <mail@substack.net>
2012-04-18 01:38:27 -0700

    now passing the az test

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 module.exports = function solve (signature, pool) {
-    return [];
+    var pre = pool.filter(function (sig) {
+        return signature[0] === sig[0];
+    });
+    if (pre.length === 0) return [];
+    
+    var exact = pool
+        .filter(function (sig) {
+            return signature[0] === sig[0] && signature[1] === sig[1];
+        })
+        .map(function (sig) {
+            return [ sig ]
+        })
+    ;
+    
+    return pre.reduce(function (acc, sig) {
+        var res = solve([ sig[1], signature[1] ], pool)
+            .map(function (r) { return [sig].concat(r) })
+        ;
+        return acc.concat(res);
+    }, exact);
 };
