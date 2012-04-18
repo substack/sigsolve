1ee6fdcf2befa2c0bb636310b536982faafb35db
James Halliday <mail@substack.net>
2012-04-18 14:54:35 -0700

    now returns the function names

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ module.exports = function solve (signature, pool) {
     if (pre.length === 0) return [];
     
     var exact = Object.keys(pool)
-        .map(function (name) {
-            return pool[name];
-        })
-        .filter(function (sig) {
+        .filter(function (name) {
+            var sig = pool[name];
             return signature[0] === sig[0] && signature[1] === sig[1];
         })
     ;
@@ -16,7 +14,7 @@ module.exports = function solve (signature, pool) {
     return pre.reduce(function (acc, name) {
         var sig = pool[name];
         var res = solve([ sig[1], signature[1] ], pool)
-            .map(function (r) { return [sig].concat(r) })
+            .map(function (r) { return [name].concat(r) })
         ;
         return acc.concat(res);
     }, exact);
