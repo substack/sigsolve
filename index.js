e268e749ceb515b227c1af451ca7dd9d67a6ada1
James Halliday <mail@substack.net>
2012-04-18 18:39:32 -0700

    function matching partly works but doesn't include or check the nested parameters

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 module.exports = function solve (signature, pool) {
+    function exactMatches (sig) {
+        if (Array.isArray(sig[0])) {
+            return exactMatches([ sig[0][0], sig[1] ]);
+        }
+        else return signature[0] === sig[0] && signature[1] === sig[1];
+    }
+    
+    function headMatches (sig) {
+        if (Array.isArray(sig[0])) {
+            return headMatches([ sig[0][0], sig[1] ]);
+        }
+        else return signature[0] === sig[0];
+    }
+    
     var pre = Object.keys(pool).filter(function (name) {
-        return signature[0] === pool[name][0];
+        var sig = pool[name];
+        return headMatches(sig[0]);
     });
     if (pre.length === 0) return [];
     
     var exact = Object.keys(pool)
         .filter(function (name) {
             var sig = pool[name];
-            return signature[0] === sig[0] && signature[1] === sig[1];
+            return exactMatches(sig);
         })
     ;
     
