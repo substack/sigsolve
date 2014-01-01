b5348ac3dafb68c0ba12d7cc3526e959c99a589a
James Halliday <mail@substack.net>
2014-01-01 12:54:27 -0800

    inline the other parameters to prevent regenerating the keys, es5 support

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-module.exports = function solve (sig, pool) {
-    var src = sig[0], dst = sig[1];
-    var keys = Object.keys(pool);
+var isArray = require('isarray');
+var objectKeys = require('object-keys');
+
+module.exports = function (sig, pool) {
+    var keys = objectKeys(pool);
+    return solve(sig[0], sig[1], pool, keys);
+};
+
+function solve (src, dst, pool, keys) {
     var matches = [];
     
     for (var i = 0; i < keys.length; i++) {
@@ -10,15 +16,15 @@ module.exports = function solve (sig, pool) {
             matches.push([ key ]);
         }
         else if (x[0] === src) {
-            var xs = solve([ x[1], dst ], pool);
+            var xs = solve(x[1], dst, pool, keys);
             for (var j = 0; j < xs.length; j++) {
                 matches.push([ key ].concat(xs[j]));
             }
         }
-        else if (Array.isArray(x[0]) && x[0][0] === src) {
-            var a = solve([ x[1], dst ], pool);
+        else if (isArray(x[0]) && x[0][0] === src) {
+            var a = solve(x[1], dst, pool, keys);
             if (!a.length) continue;
-            var b = solve(x[0], pool);
+            var b = solve(x[0][0], x[0][1], pool, keys);
             if (!b.length) continue;
             for (var j = 0; j < a.length; j++) {
                 for (var k = 0; k < b.length; k++) {
