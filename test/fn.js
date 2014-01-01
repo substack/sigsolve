eb88d77bea165e1928c9215a211b4dabc69e1d1a
James Halliday <mail@substack.net>
2014-01-01 12:48:09 -0800

    passing test for a more complicated test

--- test/fn.js
+++ test/fn.js
@@ -1,17 +1,36 @@
 var test = require('tape');
 var sigsolve = require('../');
 
-test('multiple elements in a type signature', function (t) {
+test('higher-order', function (t) {
     var pool = {
         f : [ [ 'a', 'b' ], 'c' ],
         g : [ 'c', 'z' ],
         h : [ 'a', 'd' ],
         i : [ 'd', 'b' ],
     };
-    t.same(
+    t.deepEqual(
         sigsolve([ 'a', 'z' ], pool),
         [ [ [ 'f', [ 'h', 'i' ] ], 'g' ] ]
     );
     
+    t.end();
+});
+
+test('higher-order multiple tails', function (t) {
+    var pool = {
+        f : [ [ 'a', 'b' ], 'c' ],
+        g : [ 'c', 'z' ],
+        h : [ 'a', 'd' ],
+        i : [ 'd', 'b' ],
+        j : [ 'c', 'e' ],
+        k : [ 'e', 'z' ]
+    };
+    t.deepEqual(
+        sigsolve([ 'a', 'z' ], pool),
+        [
+            [ [ 'f', [ 'h', 'i' ] ], 'g' ],
+            [ [ 'f', [ 'h', 'i' ] ], 'j', 'k' ]
+        ]
+    );
     t.end();
 });
